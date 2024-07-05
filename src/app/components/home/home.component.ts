4384068790fde4f28c35c2e423ebd1be09c24dd1
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2024-07-05 14:57:49 +0100

    updated home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  patientsCount:number = 0;
+  girlsCount:number = 0;
+  communitiesCount:number = 0;
+
+  patientsCountInterval:any = setInterval(()=>{
+    this.patientsCount += 10;
+    if(this.patientsCount >= 25000) clearInterval(this.patientsCountInterval);
+  }, 1);
+
+  girlsCountInterval:any = setInterval(()=>{
+    this.girlsCount++;
+    if(this.girlsCount == 300) clearInterval(this.girlsCountInterval);
+  }, 40);
+
+  communitiesCountInterval:any = setInterval(()=>{
+    this.communitiesCount++;
+    if(this.communitiesCount == 8) clearInterval(this.communitiesCountInterval);
+  }, 700);
 
 }
